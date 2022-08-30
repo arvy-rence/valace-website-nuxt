994f21c34e225fcf9266ecd640e6e2cca81612ea
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-08-30 13:26:18 +0800

    [ADD] Added KOHA Section in the home page

---
+++ components/section/SectionKOHA.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="text-center flex justify-center bg-gray-100 py-[3rem] ">
+        <div class="flex justify-around mx-[6rem] items-center">
+            <div class="mx-[1rem] w-1/2">
+                <h1 class="text-primary font-khula text-4xl text-right">
+                    Integrated <span class="font-bold">KOHA </span><br>Library System
+                </h1>
+                <p class="text-gray-600 font-khula text-right w-500">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a ultricies tortor. In hac habitasse platea dictumst. Curabitur non justo lacinia mauris finibus fringilla. Cras accumsan nulla et erat varius, quis sagittis tellus congue.
+                </p>
+            </div>
+            <div class="mx-[1rem] w-1/2">
+                <img src="../../assets/images/koha.png"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SectionKOHA"
+}
+</script>
+
+<style>
+
+</style>
