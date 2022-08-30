71f90a3b03ad3fa087181b7e8b1b376b5ba4e245
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-08-30 10:45:03 +0800

    Optimized random algorithm

--- components/section/SectionBookSelection.vue
+++ components/section/SectionBookSelection.vue
@@ -1,11 +1,18 @@
 <template>
-    <div class="flex justify-center items-center w-screen bg-gray-100">
-        <UtilVerticalSpacer :height="5" units="rem"/>
-        <h1 class="font-bold font-khula text-4xl text-tertiary">
+    <div class="flex-col justify-center items-center w-screen bg-gray-100 py">
+        <h1 class="font-bold font-khula text-4xl text-tertiary text-center">
             BOOK SELECTION
         </h1>
-        <div v-for="image in imageLinks">
-            <img :src="image" alt="">
+        <UtilVerticalSpacer :height="2" units="rem"/>
+        <div v-for="book in bookMetaInformation" class="py-2 px-12 flex-col">
+            <div class="">
+                <img :src="book.bookImageLink" alt="" class="mb-3 mr-5 rounded-2xl">
+                <h1 class="text-primary font-bold text-xl font-khula">{{ book.bookTitle.toUpperCase() }}</h1>
+                <span class="text-gray-600 font-khula" v-for="author in book.bookAuthor">
+                    {{ author }}
+                </span>
+                <p class="font-kulim">{{ book.bookDescription }}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -17,7 +24,9 @@ export default {
     name: "SectionBookSelection",
     data() {
         return {
-            imageLinks: []
+            bookMetaInformation: [],
+            bookMetaTitle: [],
+            testing: 1
         }
     },
     props: {
@@ -26,14 +35,43 @@ export default {
     methods: {
         async getBooksFromAPI(query) {
             const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-            console.log(response.data.items[0].volumeInfo);
-            return response.data.items[0].volumeInfo.imageLinks.thumbnail;
+            let randomIndex = Math.floor(Math.random() * response.data.totalItems);
+            console.log(response.data.totalItems)
+            console.log(randomIndex)
+
+            const randomBookData = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}?startIndex=${randomIndex}`);
+
+            const bookData = randomBookData.data.items[0].volumeInfo;
+            this.testing += 1;
+            return {
+                bookTitle: bookData.title,
+                bookAuthor: bookData.authors,
+                bookDescription: bookData.description,
+                bookImageLink: bookData.imageLinks.thumbnail
+            }
         }
     },
     async created() {
+        // for (const query of this.queryValues) {
+        //     console.log(query)
+        //     const bookInfo = await this.getBooksFromAPI(query);
+        //
+        //     if (this.bookMetaInformation.includes(bookInfo))
+        //     this.bookMetaInformation.push(await this.getBooksFromAPI(query));
+        //     console.log(this.bookMetaInformation)
+        // }
+
         for (const query of this.queryValues) {
-            this.imageLinks.push(await this.getBooksFromAPI(query));
-        }
+                console.log(query)
+                const bookInfo = await this.getBooksFromAPI(query);
+
+                if (this.bookMetaTitle.includes(bookInfo.bookTitle)) {
+                    await this.getBooksFromAPI(query);
+                }
+                this.bookMetaInformation.push(bookInfo);
+                this.bookMetaTitle.push(bookInfo.bookTitle);
+                console.log(this.bookMetaInformation)
+            }
     }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <SectionBookSelection :queryValues="generatedWords"/>
+        <SectionBookSelection :queryValues="dummy"/>
     </div>
 </template>
 
@@ -11,7 +11,12 @@ export default {
         return {
             generatedISBNNumbers: [],
             generatedWords: [],
-            name: "Rence"
+            name: "Rence",
+            dummy: [
+                'mathematics',
+                'law',
+                'business'
+            ]
         }
     },
     methods: {
@@ -32,11 +37,19 @@ export default {
              * generatedWords array
              */
             const words = [
-                "charge", "crime", "tape", "shirt", "civil", "ongoing", "game", "future", "journal", "remaining", "pretty", "whether", "probably", "silent", "positive", "stare", "information", "release", "again", "exception", "painful", "vessel", "situation", "rid", "style", "record", "wear", "basketball", "head", "lean", "shoot", "after", "least", "threaten", "folk", "sensitive", "religious", "grocery", "consumer", "massive", "dimension", "everything", "take", "ring", "administration", "need", "get", "stir", "response", "Christmas", "advantage", "long", "continue", "creature", "formula", "slow", "bite", "burn", "construction", "substantial", "can", "page", "argue", "between", "opportunity", "exist", "closely", "assistant", "public", "killer", "conflict", "typically", "necessary", "examination", "you", "labor", "photo", "cow", "fuel", "mount", "common", "around", "gang", "breast", "buyer", "learn", "thus", "moon", "victory", "investment", "imagination", "billion", "stop", "mistake", "pink", "weekend"
+                "programming", "medicine", "history", "geography", "mathematics", "philosophy", "psychology", "business", "law"
+                // "charge", "crime", "tape", "shirt", "civil", "ongoing", "game", "future", "remaining", "pretty", "whether", "probably", "silent", "positive", "stare", "information", "release", "again", "exception", "painful", "vessel", "situation", "rid", "style", "record", "wear", "basketball", "head", "lean", "shoot", "after", "least", "threaten", "folk", "sensitive", "religious", "grocery", "consumer", "massive", "dimension", "everything", "take", "ring", "administration", "need", "get", "stir", "response", "Christmas", "advantage", "long", "continue", "creature", "formula", "slow", "bite", "burn", "construction", "substantial", "can", "page", "argue", "between", "opportunity", "exist", "closely", "assistant", "public", "killer", "conflict", "typically", "necessary", "examination", "you", "labor", "photo", "cow", "fuel", "mount", "common", "around", "gang", "breast", "buyer", "learn", "thus", "moon", "victory", "investment", "imagination", "billion", "stop", "mistake", "pink", "weekend"
             ]
-            for (let i = 0; i < 3; i++) {
-                this.generatedWords.push(words[Math.floor(Math.random() * 10)]);
+            while (this.generatedWords.length < 3) {
+                let randomWord = words[Math.floor(Math.random() * words.length)];
+                if (this.generatedWords.includes(randomWord)) continue;
+                this.generatedWords.push(randomWord);
             }
+            // for (let i = 0; i < 3; i++) {
+            //     let wor = words[Math.floor(Math.random() * words.length)]
+            //     if (this.generatedWords.includes(wor)) return;
+            //     this.generatedWords.push(wor);
+            // }
         }
     },
     created() {
@@ -45,6 +58,7 @@ export default {
         //     this.generateRandomISBN(),
         //     this.generateRandomISBN()
         // ]
+        this.generatedWords = [];
         this.generateRandomWordQuery()
     }
 }
