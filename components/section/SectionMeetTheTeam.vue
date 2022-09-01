51cd608862ace42f80d717cd2a22670b899087c6
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-01 15:10:42 +0800

    [UPDATE] / Finalize Meet the Team Section (preparation for props)

---
+++ components/section/SectionMeetTheTeam.vue
@@ -0,0 +1,37 @@
+<template>
+    <UtilVerticalSpacer :height="2" units="rem"/>
+    <div class="flex flex-col justify-center items-center bg-[url(../../assets/images/book-bg.png)] py-[3rem]">
+        <h1 class="font-bold text-4xl">MEET THE TEAM</h1>
+        <UtilVerticalSpacer :height="2" units="rem"/>
+        <div class="lg:columns-4 gap-5 xs:columns-1 sm:columns-2">
+            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
+            <CardTeamInfo name="Arvy" position="Developer"/>
+            <CardTeamInfo name="Roselle" position="Graphics Artist"/>
+            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
+            <CardTeamInfo name="Arvy" position="Developer"/>
+            <CardTeamInfo name="Roselle" position="Graphics Artist"/>
+            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
+            <CardTeamInfo name="Arvy" position="Developer"/>
+        </div>
+
+    </div>
+    <UtilVerticalSpacer :height="2" units="rem"/>
+</template>
+
+<script>
+    export default {
+        name: "SectionMeetTheTeam",
+        data() {
+            return {
+
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
