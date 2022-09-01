63bfaede0322c12c83fb9c12d2dcd814cad25cc6
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-01 15:07:28 +0800

    [ADD] / Add ContactInfoSection

---
+++ components/section/SectionContactInfo.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="columns-2 px-[6rem]">
+        <div>
+            <h1 class="font-bold text-lg">
+                <i class="fa-solid fa-location-dot text-red-600"></i>    
+                Location
+            </h1>
+            <p class="pl-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex totam ipsa eum eligendi reprehenderit. Alias dicta repudiandae dolor aliquam inventore eum sunt. Aspernatur sapiente, sint quos quae blanditiis vel minima!
+            </p>
+            <h1 class="font-bold text-lg">
+                <i class="fa-solid fa-clock text-red-600"></i>
+                Library Hours
+            </h1>
+            <div class="pl-4">
+                <h2 class="font-semibold text-lg">Weekdays: 9am - 10pm</h2>
+                <h2 class="font-semibold text-lg">Saturday: 8am - 8pm</h2>
+            </div>
+
+            <h1 class="font-bold text-lg">
+                <i class="fa-solid fa-link text-red-600"></i>
+                Social Links
+            </h1>
+            <div class="pl-4">
+                <h2 class="font-semibold text-lg">
+                    <a href="https://facebook.com/valaceValenzuela" target="_blank"><i class="fa-brands fa-facebook"></i> facebook.com/valaceValenzuela</a>
+                </h2>
+                <h2 class="font-semibold text-lg">
+                    <a href="https://twitter.com/@valaceValenzuela" target="_blank"><i class="fa-brands fa-twitter"></i> @valaceValenzuela</a>
+                </h2>
+            </div>
+        </div>
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d884.7627365065613!2d120.9654603868822!3d14.692103246896254!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b58186049e65%3A0x3b293f197c2be1ab!2sValenzuela%20Academic%20Center%20for%20Excellence%20-%20ValACE!5e0!3m2!1sen!2sph!4v1661921641437!5m2!1sen!2sph" 
+                    width="600" 
+                    height="450" 
+                    style="border:0;" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade"
+                    class="shadow-lg rounded-xl">
+            </iframe>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SectionContactInfo",
+    }
+</script>
+
+<style>
+
+</style>
