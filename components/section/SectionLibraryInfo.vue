72fde17b4801a52fcff033acd8425194d4468068
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-01 15:11:22 +0800

    [ADD] / Add Library Info Section

---
+++ components/section/SectionLibraryInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="columns-2 px-[6rem]">
+        <div>
+            <img src="../../assets/images/valace-building.png">
+        </div>
+        <div class="flex flex-col items-start justify-center">
+            <h1 class="font-bold text-2xl text-primary">
+                Valenzuela City Library
+            </h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, excepturi quos. Recusandae, est. Natus iste qui accusantium, deleniti atque quidem totam eum. Ipsum mollitia quibusdam modi at quae, a est? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas exercitationem consequuntur asperiores, ipsa distinctio natus magni, iste cum atque unde, inventore ullam cupiditate dolorem id dolor reprehenderit assumenda temporibus eum? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam asperiores quod officiis, commodi corporis ut et pariatur exercitationem inventore. Tempora quasi iusto laudantium aut porro libero, cum explicabo asperiores excepturi!
+            </p>
+            <button class="bg-primary text-white rounded-xl py-3 px-12 text-xl font-bold">Read More</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SectionLibraryInfo",
+    }
+</script>
+
+<style>
+
+</style>
