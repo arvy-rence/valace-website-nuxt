1bd811892c6f7bf86d56a88eaf56e9c2bcfbd2d7
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-02 10:35:17 +0800

    [ADD] / Add data source for the CardTeamInfo component for MeetTheTeam section

---
+++ data/team.js
@@ -0,0 +1,42 @@
+export const teamInfo = [
+    {
+        name: 'Clarence Rhey Salaveria',
+        position: 'Librarian',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'James Dean San Miguel',
+        position: 'Head of IT',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Roselle Luna',
+        position: 'Graphics Designer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Jon Arvy Enriquez',
+        position: 'Developer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Roselle Luna',
+        position: 'Graphics Designer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Jon Arvy Enriquez',
+        position: 'Developer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Roselle Luna',
+        position: 'Graphics Designer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+    {
+        name: 'Jon Arvy Enriquez',
+        position: 'Developer',
+        imageURL: '../../assets/images/people/dummy-1.png'
+    },
+]
