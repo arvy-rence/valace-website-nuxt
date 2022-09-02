16e463f6f4dfcf1a78779c62b01c1e93ff5694fa
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-02 10:33:02 +0800

    [UPDATE] / Add responsiveness to KOHA section

--- components/section/SectionKOHA.vue
+++ components/section/SectionKOHA.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="text-center flex justify-center bg-gray-100 py-[3rem]">
-        <div class="flex justify-around mx-[6rem] items-center">
-            <div class="mx-[1rem] w-1/2">
-                <h1 class="text-primary font-khula text-4xl text-right">
-                    Integrated <span class="font-bold">KOHA </span><br>Library System
-                </h1>
-                <p class="text-gray-600 font-khula text-right w-500">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a ultricies tortor. In hac habitasse platea dictumst. Curabitur non justo lacinia mauris finibus fringilla. Cras accumsan nulla et erat varius, quis sagittis tellus congue.
-                </p>
-            </div>
-            <div class="mx-[1rem] w-1/2">
-                <img src="../../assets/images/koha.png"/>
-            </div>
+    <div class="flex flex-col-reverse lg:flex-row px-[6rem] py-[4rem]">
+        <div class="w-full lg:w-1/2 px-[1rem] mt-[2rem]">
+            <h1 class="text-primary font-khula text-4xl text-right">
+                Integrated <span class="font-bold">KOHA </span><br>Library System
+            </h1>
+            <p class="text-gray-600 font-khula text-right w-500">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a ultricies tortor. In hac habitasse platea dictumst. Curabitur non justo lacinia mauris finibus fringilla. Cras accumsan nulla et erat varius, quis sagittis tellus congue.
+            </p>
+        </div>
+        <div class="w-full lg:w-1/2">
+            <img src="../../assets/images/koha.png" class="w-full"/>
         </div>
     </div>
 </template>
