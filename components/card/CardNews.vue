0df92442c63afbd74bafb46ec31ea1878bd6765a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-02 10:40:03 +0800

    [UPDATE] / Finalized News Card component

--- components/card/CardNews.vue
+++ components/card/CardNews.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-[22rem] bg-gray mx-4 minmax-h-[40rem]">
-        <div class="bg-white-100 custom-bottom-shadow img-with-shadow my-[.7rem] px-[1.4rem] pt-4">
+    <div class="w-[22rem] bg-gray mx-4 ">
+        <div class="bg-white-100 custom-bottom-shadow img-with-shadow my-[.7rem] px-[1.4rem] pt-4 min-h-[24rem]">
             <figure class="flex justify-center items-center py-[.5rem]">
-                <img src="../../assets/images/icons/logo.png" width=75/>
+                <img src="../../assets/images/icons/logo.png" width="75" alt="">
             </figure>
             <figure class="flex justify-center items-center">
-                <img :src="newsInfo.newsImageURL" width="340"/>
+                <img :src="newsInfo.newsImageURL" width="340" alt=""/>
             </figure>
             <div class="text-gray-400 font-kulim pt-4">
                 <i class="fa-solid fa-clock"></i> {{ dummyDateString }}
@@ -18,9 +18,11 @@
                 </p>
             </div>
             <div class="inline-flex items-center mt-5 pb-5">
-                <NuxtLink :to="newsInfo.newsLink" class="text-primary text-sm underline font-bold">
-                    <button class="bg-primary rounded-md px-4 py-2">
-                        <span class="text-white">
+                <NuxtLink :to="newsInfo.newsLink"
+                          target="_blank"
+                          class="text-primary text-sm underline font-bold">
+                    <button class="py-2">
+                        <span class="text-primary hover:underline">
                             <i class="fa-solid fa-circle-chevron-down"></i>
                             Read More
                         </span>
@@ -42,6 +44,7 @@ export default {
     props: {
         /**
          * newsInfo should have the following format:
+         *     newsId: number (not required in this component)
          *     newsTitle: string;
          *     newsDate: Date;
          *     newsContent: string;
