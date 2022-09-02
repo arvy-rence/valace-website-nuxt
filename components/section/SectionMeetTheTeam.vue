ee4d711b46061402e7e483927e322e2fbf034e8a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-02 10:41:57 +0800

    [ADD] / Add v-for directive and integrated data from team.js

--- components/section/SectionMeetTheTeam.vue
+++ components/section/SectionMeetTheTeam.vue
@@ -1,33 +1,33 @@
 <template>
-    <UtilVerticalSpacer :height="2" units="rem"/>
-    <div class="flex flex-col justify-center items-center bg-[url(../../assets/images/book-bg.png)] py-[3rem]">
+    <div class="flex flex-col justify-center items-center bg-[url(../../assets/images/book-bg.png)] py-[3rem] bg-cover">
         <h1 class="font-bold text-4xl">MEET THE TEAM</h1>
         <UtilVerticalSpacer :height="2" units="rem"/>
-        <div class="lg:columns-4 gap-5 xs:columns-1 sm:columns-2">
-            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
-            <CardTeamInfo name="Arvy" position="Developer"/>
-            <CardTeamInfo name="Roselle" position="Graphics Artist"/>
-            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
-            <CardTeamInfo name="Arvy" position="Developer"/>
-            <CardTeamInfo name="Roselle" position="Graphics Artist"/>
-            <CardTeamInfo name="Clarence Rhey Barraca Salaveria" position="Librarian"/>
-            <CardTeamInfo name="Arvy" position="Developer"/>
+        <div class="gap-5 lg:columns-4 xs:columns-1 sm:columns-3">
+            <div v-for="member in teamInfoArray">
+                <CardTeamInfo
+                    :name="member.name"
+                    :position="member.position"
+                    :imageURL="member.imageURL"
+                />
+            </div>
         </div>
-
     </div>
-    <UtilVerticalSpacer :height="2" units="rem"/>
 </template>
 
 <script>
+    import { teamInfo } from "../../data/team";
+    import CardTeamInfo from "../card/CardTeamInfo";
+
     export default {
         name: "SectionMeetTheTeam",
+        components: {CardTeamInfo},
         data() {
             return {
-
+                teamInfoArray: []
             }
         },
         created() {
-
+            this.teamInfoArray = teamInfo;
         }
     }
 </script>
