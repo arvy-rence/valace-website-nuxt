e6f938871e4bb6443a5271358d4b78b2ddcc64ea
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-06 08:47:53 +0800

    [ADD] / Add index route for news

---
+++ pages/news/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="bg-white-100 flex-col bg-img justify-center align-center">
+        <UtilVerticalSpacer :height="2" units="rem"/>
+        <div>
+            <h2 class="font-bold text-lg text-gray-400 font-khula text-center">
+                Valenzuela City Library
+            </h2>
+            <h1 class="font-bold text-4xl text-primary text-center font-khula">
+                NEWS & ANNOUNCEMENTS
+            </h1>
+        </div>
+        <div class="flex justify-center flex-wrap">
+            <div v-for="newsInfo in newsData" :key="newsInfo.id">
+                <CardNews :newsInfo="newsInfo"/>
+            </div>
+        </div>
+        <div class="flex justify-center items-center py-[2rem]">
+            <button
+                class="bg-primary px-12 py-3 rounded-md text-white font-bold font-khula text-lg"
+            >
+                View All News
+            </button>
+        </div>
+        <UtilVerticalSpacer :height="2" units="rem"/>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import CardNews from "../../components/card/CardNews";
+import {checkReload} from "../../composables/checkReload";
+
+export default {
+    name: "index",
+    components: {CardNews},
+    data() {
+        return {
+            newsData: []
+        }
+    },
+    async created() {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        console.log(response.data);
+        response.data.forEach((news) => {
+            this.newsData.push({
+                id: news.id,
+                newsTitle: news.title,
+                newsDate: new Date().toDateString(),
+                newsContent: news.body,
+                newsImageURL: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80",
+                newsLink: `/news/${news.id}`
+            })
+        })
+    },
+    mounted() {
+        checkReload()
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
