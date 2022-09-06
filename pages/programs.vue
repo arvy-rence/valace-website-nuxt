7267d3fade26e8c93519098934e9ac1aad05052c
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-06 08:50:34 +0800

    [ADD] / Add programs and sister-cities page

---
+++ pages/programs.vue
@@ -0,0 +1,15 @@
+<template>
+    <SectionPrograms/>
+</template>
+
+<script>
+export default {
+    name: "programs",
+    components: {}
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/sister-cities.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+import {checkReload} from "../composables/checkReload";
+
+export default {
+    name: "sister-cities",
+    mounted() {
+        checkReload()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
