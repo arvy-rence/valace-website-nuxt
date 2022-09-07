c3ee507d76a3281f766522f42d4d0b3a235bd83d
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-07 09:21:28 +0800

    [UPDATE] / Adjusted image URL to use in public dir

--- data/facilityInfo.js
+++ data/facilityInfo.js
@@ -8,25 +8,25 @@ export const facilityInfo = [
     {
         title: "Arts Education Office",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
-        image: "../../assets/images/icons/arts.png",
+        image: "assets/images/icons/arts.png",
         floorInfo: "1st Floor"
     },
     {
         title: "Teacher's Training Center",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
-        image: "../../assets/images/icons/training.png",
+        image: "assets/images/icons/training.png",
         floorInfo: "4th to 5th Floor"
     },
     {
         title: "SPED Annex",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
-        image: "../../assets/images/icons/sped.png",
+        image: "assets/images/icons/sped.png",
         floorInfo: "6th Floor"
     },
     {
         title: "Education360 Office",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
-        image: "../../assets/images/icons/educ.png",
+        image: "assets/images/icons/educ.png",
         floorInfo: "6th Floor"
     }
 ]
