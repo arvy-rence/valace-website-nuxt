9d9d40a1ea75d9ecc628f80795e0d8ca43d89286
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-08 08:46:53 +0800

    [UPDATE] / Add breakpoint class to implement responsiveness

--- components/section/SectionTopnotchers.vue
+++ components/section/SectionTopnotchers.vue
@@ -1,7 +1,7 @@
 <template>
     <UtilVerticalSpacer :height="3" units="rem"/>
-    <div class="grid grid-cols-8 gap-4">
-        <div class="col-start-3 col-span-2 inline-flex flex-col justify-center">
+    <div class="flex flex-col-reverse md:flex-row px-[2rem] lg:px-[6rem] md:justify-center">
+        <div class="inline-flex flex-col justify-center pt-[1rem] lg:pt-0 md:pr-[2rem]">
             <h1 class="text-4xl text-primary font-khula font-bold">
                 TOPNOTCHERS' SHELF
             </h1>
@@ -12,8 +12,7 @@
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis iure repellat incidunt quidem adipisci architecto. Sapiente delectus amet, eaque esse labore ipsum beatae natus vel perferendis, voluptatem vero est distinctio?
             </p>
         </div>
-        <img :src="image" width="200" alt=""
-            class="img-with-shadow rounded-xl col-start-6">
+        <img :src="image" class="img-with-shadow rounded-xl w-full md:w-[14rem]">
     </div>
     <UtilVerticalSpacer :height="3" units="rem"/>
 </template>
