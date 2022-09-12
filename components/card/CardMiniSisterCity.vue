8491174f307b2311fba1e2ad58d2cd662582c276
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-09-12 08:49:10 +0800

    [ADD] / Update SisterCity section and add minicards for the updated UI

---
+++ components/card/CardMiniSisterCity.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card-class px-[2rem] sm:px-[5rem] md:px-[10rem] lg:px-[20rem] xl:px-[25rem] py-2">
+        <div class="bg-white border border-gray-300 flex items-center justify-start px-3 py-3">
+            <img src="/images/icons/logo.png" alt="" class="py-2" width="100">
+            <div class="flex flex-col">
+                <span class="pl-5 font-khula text-xl font-bold">{{ sisterCityInfo.city }}</span>
+                <span class="pl-5 font-kulim">
+                    <i class="fa-solid fa-location-pin"></i>
+                    {{ sisterCityInfo.address }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CardMiniSisterCity",
+    props: {
+        sisterCityInfo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+@media(min-width: 1024px) {
+    .card-class {
+        padding: 16px 12rem 16px 12rem;
+    }
+}
+</style>
--- components/section/SectionSisterCities.vue
+++ components/section/SectionSisterCities.vue
@@ -6,64 +6,64 @@
             <h1 class="font-bold text-4xl text-primary text-center font-khula">SISTER CITIES<br>
                 VISITING CITIES' CORNER
             </h1>
-        </div>
-
-        <div class="flex justify-center">
-            <!-- card component -->
-            <div class="w-[22rem] bg-gray">
-                <div class="bg-gray-100 rounded-xl img-with-shadow my-[.7rem] px-[.5rem] py-16">
-                    <figure class="flex justify-center items-center py-[.5rem]">
-                        <img src="/images/icons/logo.png" width=75/>
-                    </figure>
-                    <figure class="flex justify-center items-center">
-                        <img src="/images/koha.png" width="300"/>
-                    </figure>
-                    <h1 class="font-bold text-md text-black-100 py-[.3rem] text-center font-khula">Valenzuela City Library</h1>
-                    <div class="text-[12px] text-black-100 text-center font-khula">
-                        <i class="fa-solid fa-location-pin"></i>
-                        <span>
-                            123# ASDASD ST. MALINTA VALENZUELA CITY
-                        </span>
-                    </div>
+            <div class="flex justify-center">
+                <div class="w-1/2">
+                    <p class="font-kulim text-xl">
+                        The Sister City, Sister Library Initiative is a program that aims to foster the ties between
+                        Valenzuela
+                        City and its sister municipality, Baliwag, through public library partnership.
+                    </p>
                 </div>
             </div>
         </div>
-
-        <div class="flex justify-center items-center py-[2rem]">
-            pagination component here
+        <div class="py-6">
+            <div class="flex flex-col justify-center flex-nowrap overflow-hidden">
+                <!-- card component -->
+                <div v-for="(info, index) in sisterCitiesData" :key="index">
+                    <CardMiniSisterCity :sisterCityInfo="info"/>
+                </div>
+            </div>
+            <UtilVerticalSpacer :height="2" units="rem"/>
+            <div class="flex justify-center items-center">
+                <NuxtLink to="/sister-cities/">
+                    <button
+                        class="bg-primary px-12 py-3 rounded-md text-white font-bold font-khula text-lg"
+                    >
+                        View Sister Cities
+                    </button>
+                </NuxtLink>
+            </div>
+            <UtilVerticalSpacer :height="2" units="rem"/>
         </div>
-        <div class="flex justify-center items-center">
-            <NuxtLink to="/sister-cities/">
-                <button
-                    class="bg-primary px-12 py-3 rounded-md text-white font-bold font-khula text-lg"
-                >
-                    View Sister Cities
-                </button>
-            </NuxtLink>
-        </div>
-        <UtilVerticalSpacer :height="2" units="rem"/>
     </div>
 </template>
 
 <script>
-import UtilPagination from "../util/UtilPagination";
+import {sisterCities} from "../../data/sisterCities";
+import CardMiniSisterCity from "../card/CardMiniSisterCity";
+
 export default {
     name: "SectionSisterCities",
-    components: {UtilPagination},
-    methods: {
-        onClick() {
-            alert("Hello")
+    components: {CardMiniSisterCity},
+    data() {
+        return {
+            sisterCitiesData: []
         }
-    }
+    },
+    async created() {
+        for (let i = 0; i < 6; i++) {
+            this.sisterCitiesData.push(sisterCities[i])
+        }
+    },
 }
 </script>
 
 <style>
-    .bg-img {
-        background-size: cover;
-    }
+.bg-img {
+    background-size: cover;
+}
 
-    .img-with-shadow {
-        box-shadow: 2px 2px 10px gray;
-    }
+.img-with-shadow {
+    box-shadow: 2px 2px 10px gray;
+}
 </style>
