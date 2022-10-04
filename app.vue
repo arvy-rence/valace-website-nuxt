321d6afa5bb30325b39408b3bac7206a85c4e2f2
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-10-04 13:46:40 +0800

    [CONFIG] / Change config files

--- app.vue
+++ app.vue
@@ -12,6 +12,23 @@
             {
                 rel: "stylesheet",
                 href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css"
+            }
+        ],
+
+        script: [
+            {
+                src: "https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"
+            },
+            {
+                src: "https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"
             }
         ]
     })
