77e78f64b90edd9ebeddf52c4e88e64754a60b59
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-10-04 13:47:02 +0800

    [UPDATE] / Added changes to the data

--- data/facilityInfo.js
+++ data/facilityInfo.js
@@ -12,6 +12,12 @@ export const facilityInfo = [
         floorInfo: "1st Floor"
     },
     {
+        title: "Valenzuela Live Studio",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
+        image: "/images/icons/live.png",
+        floorInfo: "4th Floor"
+    },
+    {
         title: "Teacher's Training Center",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at maximus dolor, sed volutpat felis.",
         image: "/images/icons/training.png",
--- data/team.js
+++ data/team.js
@@ -1,41 +1,41 @@
 export const teamInfo = [
     {
-        name: 'A',
+        name: 'Rochelle Silverio',
         position: '',
         imageURL: '/images/team/1.jpg',
     },
     {
-        name: 'A',
+        name: 'April Beranque',
         position: '',
         imageURL: '/images/team/2.jpg'
     },
     {
-        name: 'A',
+        name: 'Ann Patricia Badique',
         position: '',
         imageURL: '/images/team/3.jpg'
     },
     {
-        name: 'A',
+        name: 'Maribel Ladera',
         position: '',
         imageURL: '/images/team/4.jpg'
     },
     {
-        name: 'A',
+        name: 'Donnalyn Estrada',
         position: '',
         imageURL: '/images/team/5.jpg'
     },
     {
-        name: 'A',
+        name: 'James Dean San Miguel',
         position: '',
         imageURL: '/images/team/6.jpg'
     },
     {
-        name: 'A',
+        name: 'Joyce Ann Artacho',
         position: '',
         imageURL: '/images/team/7.jpg'
     },
     {
-        name: 'A',
+        name: 'Roselle Luna',
         position: '',
         imageURL: '/images/team/8.jpg'
     },
