5718bd82f4cda4f28af239af2ddb1f65233032b8
Rence Salaveria <rencesalaveria@outlook.com>
2022-10-11 19:29:28 -0700

    [DEPS] / Deps and test files

--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,10 @@
 <template>
     <div class="pt-[82.8px]"></div>
-    <SectionNewHeader/>
 </template>
 
 <script>
-import Testing from "../components/section/Testing";
-import SectionTopnotchers from "../components/section/SectionTopnotchers";
-import SectionEvents from "../components/section/SectionEvents";
-import SectionNewBookSelection from "../components/section/SectionNewBookSelection";
-import SectionPrograms from "../components/section/SectionPrograms";
-import SectionProgramsSisterCities from "../components/section/SectionProgramsSisterCities";
-import SectionNewHeader from "../components/section/SectionNewHeader.vue"
-
 export default {
-    components: {SectionNewHeader}
+    
 }
 </script>
 
