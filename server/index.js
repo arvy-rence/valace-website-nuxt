02df573f022509a2df65d07ac1d7945e799944d8
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-10-28 14:10:15 +0800

    [UPDATE] / Updated API URL

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:"https://valace-api.herokuapp.com/api",
+    // baseURL:"https://valace-api.herokuapp.com/api",
+    baseURL:"https://valace-app.herokuapp.com/api"
     // baseURL:"http://localhost:8080/api",
 });
