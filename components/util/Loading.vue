c255f1203bd218d48438b9c279be5253a02c9a68
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-11-06 18:20:18 +0800

    [ADD] / Loading screen component

---
+++ components/util/Loading.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-screen h-screen flex justify-center items-center z-10">
+        <img src="/images/loading/flipPageTry.gif" alt="" class="block w-[150px]">
+    </div>
+</template>
+
+<script>
+
+export default {
+}
+
+</script>
+
+<style scoped>
+</style>
