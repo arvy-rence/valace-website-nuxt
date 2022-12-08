01b97fe0073e4e7adb613495b986f08f2071c5ab
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2022-12-08 19:28:50 +0800

    [UPDATE] / Change baseURL for the API

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL:"https://valace-api.herokuapp.com/api",
-    baseURL:"https://valace-app.herokuapp.com/api"
+    baseURL:"https://vcl-website.herokuapp.com/api",
     // baseURL:"http://localhost:8080/api",
 });
